ffdec9f9d3aec03db9f6b580d9be95b75daeb3f3
Mauro <mauro3@runbox.com>
2024-06-10 13:33:49 +0200

    Added net balance functions
    This is untested, please see whether this works.

--- melt.py
+++ melt.py
@@ -17,6 +17,55 @@ def lapsed_temperature(temperature_s, elevation, lapse_rate):
     delta_h = elevation - elevation_s
     return lapse_rate * delta_h + temperature_s
 
+def net_balance_fn(dt, Ts, Ps, melt_factor, T_threshold):
+    """
+    Integrate the balance rate (this is at a point) over time for given temperature and precipitation arrays to get the "net balance".
+
+    Args:
+        dt: The time step.
+        Ts: Array of temperatures.
+        Ps: Array of precipitations.
+        melt_factor: The factor to compute melt amount.
+        T_threshold: The temperature threshold for accumulation.
+
+    Returns:
+        net balance (this is at a point)
+    """
+    assert len(Ts) == len(Ps)
+    total = 0.0
+    for T, P in zip(Ts, Ps):
+        balance_rate = -melt(T, melt_factor) + accumulate(T, P, T_threshold)
+        total += balance_rate * dt
+    return total
+
+
+def glacier_net_balance_fn(zs, dt, Ts, Ps, melt_factor, T_threshold, lapse_rate):
+    """
+    Calculate:
+    - the glacier net balance (integration of balance rate over time and space)
+    - the net balance at each point (integration of balance rate over time)
+
+    Args:
+        zs: Array of elevations (with the weather station as datum)
+        dt: The time step.
+        Ts: Array of temperatures.
+        Ps: Array of precipitations.
+        melt_factor: The factor to compute melt amount.
+        T_threshold: The temperature threshold for accumulation.
+        lapse_rate: The lapse rate (temperature change per unit elevation change).
+
+    Returns:
+        the glacier net balance [m]
+        net balance at all points [m]
+    """
+    glacier_net_balance = 0.0
+    net_balance = np.zeros(len(zs))
+    for i, z in enumerate(zs):
+        TT = [lapse(T, z, lapse_rate) for T in Ts]
+        net_balance[i] = net_balance_fn(dt, TT, Ps, melt_factor, T_threshold)
+        glacier_net_balance += net_balance[i]
+    return glacier_net_balance / len(zs), net_balance
+
 
 # Example usage
 temperature_s = 5    # °C, temperature at weather station
